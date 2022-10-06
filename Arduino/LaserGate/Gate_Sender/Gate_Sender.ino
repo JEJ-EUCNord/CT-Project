ebe36c5f98ceabe61f5772ea5da5f40e54babe52
Jens Jensen <jej@eucnord.dk>
2022-10-06 18:52:52 +0200

    Create Gate_Sender.ino
    Added code for the sending gate

---
+++ Arduino/LaserGate/Gate_Sender/Gate_Sender.ino
@@ -0,0 +1,59 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define BUZZER A3
+#define LASER_ADC A1
+#define PHOTODIODE A2
+#define LASER_PWM 3
+#define LED_GREEN 8
+#define LED_RED 7
+
+RF24 radio(6, 4);  // CE, CSN
+const byte address[6] = "00001";
+char sender = 'A';
+
+void setup() {
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(PHOTODIODE, INPUT);
+
+  Serial.begin(9600);
+
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+
+  // Selftest !?!?!?!
+  for (int i = 0; i < 20; i++) {
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_RED, HIGH);
+    delay(100);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_RED, LOW);
+    delay(100);
+  }
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+}
+
+void beep() {
+  digitalWrite(BUZZER, HIGH);
+  delay(20);
+  digitalWrite(BUZZER, LOW);
+}
+
+void loop() {
+  if (digitalRead(PHOTODIODE) == LOW) {
+    radio.write(&sender, sizeof(sender));
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    beep();
+    delay(3000);
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+  }
+}
